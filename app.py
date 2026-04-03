c0111d6532bf9a55f6f31cc0f6cd4d459b6635d2
Narakamol.S <kailikatty@gmail.com>
2026-04-04 03:56:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template
 from routes.chat_routes import chat_bp
 from flask_cors import CORS
+from google import genai 
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-import os
-from google import genai   # ✅ ต้อง import แบบนี้
-
-client = genai.Client(
-    api_key=os.environ.get("GEMINI_API_KEY")
-)
-
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))  # ✅ ต้อง import แบบนี้
 # register route
 app.register_blueprint(chat_bp)
 
